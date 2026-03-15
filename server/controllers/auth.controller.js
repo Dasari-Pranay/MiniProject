df179e657f1fb967e45cdc2695973fbe840e29e1
Pranay Dasari <dasaripranaynetha@gmail.com>
2026-03-15 20:25:40 +0530

    Update cookie settings for secure token handling

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -70,8 +70,8 @@ export const loginUser = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -107,8 +107,8 @@ export const googleAuth = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
